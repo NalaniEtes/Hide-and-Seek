992ca29280f4bbe16bbd4af956b7145507a4b1a2
NalaniEtes <nanijetes@gmail.com>
2025-10-25 20:27:29 +0000

    done teehee

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -1,7 +1,50 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class App {
+
+  public static int linearSearch(ArrayList<Fuzzy> fuzzies) {
+    for (int i = 0; i < fuzzies.size(); i++) {
+      if (fuzzies.get(i).color.equals("gold")) {
+        return i;
+      }
+    }
+    return -1;
+  }
+
+  public static int binarySearch(ArrayList<Fuzzy> fuzzies) {
+    int left = 0;
+    int right = fuzzies.size() -1;
+
+    while (left <= right) {
+      int mid = (left + right) / 2;
+      String midColor = fuzzies.get(mid).color;
+
+      if (midColor.equals("gold")) {
+        return mid;
+      } else if (midColor.compareTo("gold") < 0) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
+    return -1;
+  }
+
   public static void main(String[] args) {
-    System.out.println("Your code goes here!");
+    FuzzyListGenerator generator = new FuzzyListGenerator(1000);
+    ArrayList<Fuzzy> sortedFuzzies = generator.sortedRainbowFuzzies();
+    ArrayList<Fuzzy> randomFuzzies = generator.randomizedRainbowFuzzies();
+
+    int testOne = linearSearch(sortedFuzzies);
+    int testTwo = binarySearch(sortedFuzzies);
+    int testThree = linearSearch(randomFuzzies);
+    int testFour = binarySearch(randomFuzzies);
+
+    System.out.println("Linear Search on sorted list: " + testOne);
+    System.out.println("Binary Search on sorted list: " + testTwo);
+    System.out.println("Linear Search on random list: " + testThree);
+    System.out.println("Binary Search on random list: " + testFour);
   }
 }
--- app/src/test/java/org/example/AppTest.java
+++ app/src/test/java/org/example/AppTest.java
@@ -1,11 +1,43 @@
 package org.example;
 
 import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.*;
 
-class AppTest {
+public class AppTest {
   @Test
-  void theTestsWork() {
-    assertEquals(true, true);
+  public void testBinarySearchSorted() {
+    FuzzyListGenerator generator = new FuzzyListGenerator(1000);
+    ArrayList<Fuzzy> sortedFuzzies = generator.sortedRainbowFuzzies();
+
+    int index = App.binarySearch(sortedFuzzies);
+
+    assertTrue(index >= 0 && index < sortedFuzzies.size());
+    assertEquals("gold", sortedFuzzies.get(index).color);
+  }
+
+  @Test
+  public void testLinearSearchRandom() {
+    FuzzyListGenerator generator = new FuzzyListGenerator(1000);
+    ArrayList<Fuzzy> randomFuzzies = generator.randomizedRainbowFuzzies();
+
+    int index = App.linearSearch(randomFuzzies);
+
+    assertTrue(index >= 0 && index < randomFuzzies.size());
+    assertEquals("gold", randomFuzzies.get(index).color);
+  }
+
+  @Test
+  public void testBinarySearchRandom() {
+    FuzzyListGenerator generator = new FuzzyListGenerator(1000);
+    ArrayList<Fuzzy> randomFuzzies = generator.randomizedRainbowFuzzies();
+
+    int index = App.binarySearch(randomFuzzies);
+
+    if (index != -1) {
+      assertEquals("gold", randomFuzzies.get(index).color);
+    } else {
+      assertEquals(-1, index);
+    }
   }
 }
